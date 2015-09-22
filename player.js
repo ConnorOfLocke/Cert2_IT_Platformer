d6ffa7553c16f8316d71b21ceb20c640b396d095
Connor Locke-Warburton <clocke555@gmail.com>
2015-09-22 21:40:06 +1000

    Sick FX
    +Added Jump sound
    +Added Background
    + Added prev weeks code

--- player.js
+++ player.js
@@ -1,9 +1,64 @@
+var LEFT = 0;
+var RIGHT = 1;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_SHOOT_LEFT = 6;
+var ANIM_SHOOT_RIGHT = 7;
+var ANIM_MAX = 8;
 
 var Player = function() {	
-	this.image = document.createElement("img");
+	// delete me
+	//this.image = document.createElement("img");
+	//this.image.src = "hero.png";   
+
+	this.sprite = new Sprite("ChuckNorris.png");
+	
+	//idle left
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+							[0,1,2,3,4,5,6,7]);
+	//jump left
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+							[8,9,10,11,12]);
+	//walk left
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+							[13,14,15,16,17,18,19,20,21,22,23,24,25]);
+							
+	//idle right
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+							[52,53,54,55,56,57,58,59]);
+	//jump right
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+							[60,61,62,63,64]);
+	//walk right
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 	
+							[66,67,68,69,70,71,72,73,74,75,76,77,78]);
+
+	//shoot left
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 		
+							[27,28,29,30,31,32,33,34,35,36, 37, 38]);
+	//shoot right
+	this.sprite.buildAnimation(12,8, 165, 126, 0.05,
+							[79,80,81,82,83,84,85,86,87,88,89,90,91,92]);
+							
+	//offset
+	for (var i =0; i < ANIM_MAX; i++)
+	{
+		this.sprite.setAnimationOffset(i, -55, -87);
+	}
+	
+	this.direction = LEFT;
+	this.shooting = false;
 	
 	this.x = 9 * TILE;
 	this.y = 0 * TILE; 	
+	
+	this.start_x = this.x;
+	this.start_y = this.y;
 	
 	this.width = 159;
 	this.height = 163;	
@@ -17,11 +72,29 @@ var Player = function() {
 	this.falling = true;
 	this.jumping = false;
 	
-	this.image.src = "hero.png";   
+	this.lives = 3;
+	this.lives_image = document.createElement("img");
+	this.lives_image.src = "heart.png";
+	
+	////////////////////////////////////
+	var self = this;
+	this.jump_sfx_isPlaying = false;
+	this.jump_sfx = new Howl(
+	{
+		urls : ["fireEffect.ogg"],
+		buffer : true,
+		volume : 1,
+		onend: function() {
+			self.jump_sfx_isPlaying = false;
+		}
+	});
+	////////////////////////////////////////
 };
 
 Player.prototype.update = function(deltaTime)
-{		
+{	
+	this.sprite.update(deltaTime);
+
 	var left = false;
 	var right = false;
 	var jump = false;
@@ -29,16 +102,68 @@ Player.prototype.update = function(deltaTime)
 	if (keyboard.isKeyDown(keyboard.KEY_LEFT))
 	{
 		left = true;
+		this.direction = LEFT;
+		if (this.sprite.currentAnimation != ANIM_WALK_LEFT
+			&& this.sprite.currentAnimation != ANIM_SHOOT_LEFT
+			&& !this.jumping)
+			this.sprite.setAnimation(ANIM_WALK_LEFT);
 	}
-	if (keyboard.isKeyDown(keyboard.KEY_RIGHT))
+	else if (keyboard.isKeyDown(keyboard.KEY_RIGHT))
 	{
 		right = true;
+		this.direction = RIGHT;
+		if (this.sprite.currentAnimation != ANIM_WALK_RIGHT
+			&& this.sprite.currentAnimation != ANIM_SHOOT_RIGHT
+			&& !this.jumping)
+			this.sprite.setAnimation(ANIM_WALK_RIGHT);
 	}
+	else
+	{
+		if (!this.jumping && !this.falling && !this.shooting)
+		{
+			if (this.direction == LEFT)
+			{
+				if (this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+					this.sprite.setAnimation(ANIM_IDLE_LEFT);
+			}
+			else
+			{
+				if (this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+					this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+			}
+		}
+	}
+	
 	if (keyboard.isKeyDown(keyboard.KEY_SPACE))
 	{
 		jump = true;
-	}
+		if (left) 
+			this.sprite.setAnimation(ANIM_JUMP_LEFT);
+		
+		if (right)
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+		
 
+	}
+	
+	if (keyboard.isKeyDown(keyboard.KEY_SHIFT) && !jump)
+	{
+		this.shooting = true;
+		if (this.direction == LEFT)
+		{
+			if (this.sprite.currentAnimation != ANIM_SHOOT_LEFT )
+				this.sprite.setAnimation(ANIM_SHOOT_LEFT);
+		}
+		else
+		{
+			if (this.sprite.currentAnimation != ANIM_SHOOT_RIGHT)
+				this.sprite.setAnimation(ANIM_SHOOT_RIGHT);
+		}
+	}
+	else if (this.shooting)
+		this.shooting = false;
+	
+	
 	var wasleft = (this.velocity_x < 0);
 	var wasright = (this.velocity_x > 0);
 	var falling = this.falling;
@@ -56,10 +181,21 @@ Player.prototype.update = function(deltaTime)
 	else if (wasright)
 		ddx = ddx - FRICTION;
 	
-	if (jump && !this.jumping && !falling)
+	if (jump && !this.jumping && !falling) 
 	{
 		ddy = ddy - JUMP;
 		this.jumping = true;
+		if (this.direction == LEFT)
+			this.sprite.setAnimation(ANIM_JUMP_LEFT);
+		else
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+		
+		if (!this.jump_sfx_isPlaying)
+		{	
+			this.jump_sfx.play();
+			this.jump_sfx_isPlaying = true;
+		}
+		
 	}
 	
 	this.x = Math.floor( this.x + (deltaTime * this.velocity_x));
@@ -127,13 +263,34 @@ Player.prototype.update = function(deltaTime)
 			this.velocity_x = 0;
 		}
 	}
+	
+	if (this.y > canvas.height	+ 300)
+	{
+		this.lives --;
+		this.x = this.start_x;
+		this.y = this.start_y;
+	}
 }
 
-Player.prototype.draw = function()
+Player.prototype.draw = function(cam_x, cam_y)
 {
-	context.save();			
-		context.translate(this.x, this.y);
-		context.rotate(this.rotation);
-		context.drawImage(this.image, this.offset_x, this.offset_y);	
-	context.restore();	
+	this.sprite.draw(context, this.x - cam_x, this.y - cam_y);
+		
+	for (var i = 0; i < this.lives; i++)
+	{
+		context.save();
+			context.translate(50 + ( 5 + this.lives_image.width) * i, 40);
+			context.drawImage(this.lives_image,
+				-this.lives_image.width/2, -this.lives_image.height/2,
+				this.lives_image.width, this.lives_image.height);
+		context.restore();
+	}
 }
+
+
+
+
+
+
+
+
